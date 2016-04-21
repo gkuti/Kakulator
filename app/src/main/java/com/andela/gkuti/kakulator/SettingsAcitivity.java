e097354addf8bc23e1dbf8aab2398c01dbca416a
andela-gkuti <kuti.gbolahan@andela.com>
2016-04-21 16:06:42 +0100

    Create SettingsActivity class

---
+++ app/src/main/java/com/andela/gkuti/kakulator/SettingsAcitivity.java
@@ -0,0 +1,12 @@
+package com.andela.gkuti.kakulator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SettingsAcitivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
