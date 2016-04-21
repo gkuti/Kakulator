f7b92e6f72e583f8f60da0afc29dd381b3795608
andela-gkuti <kuti.gbolahan@andela.com>
2016-04-21 14:39:28 +0100

    [Finishes #118079011]Refactor RateFetcher class

--- app/src/main/java/com/andela/gkuti/kakulator/RateFetcher.java
+++ app/src/main/java/com/andela/gkuti/kakulator/RateFetcher.java
@@ -4,14 +4,11 @@ import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
-import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class RateFetcher extends AsyncTask<URL, String, String> {
@@ -25,6 +22,8 @@ public class RateFetcher extends AsyncTask<URL, String, String> {
     private Double exchangeRate;
     private DataStore dataStore;
     private String[] rate;
+    private StringBuffer buffer = new StringBuffer();
+    private String line = "";
 
     public RateFetcher(Activity activity) {
         this.activity = activity;
@@ -52,8 +51,20 @@ public class RateFetcher extends AsyncTask<URL, String, String> {
             connection.connect();
             stream = connection.getInputStream();
             reader = new BufferedReader(new InputStreamReader(stream));
-            StringBuffer buffer = new StringBuffer();
-            String line = "";
+            return parseJson();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        progressDialog.dismiss();
+    }
+
+    private String parseJson() {
+        try {
             while ((line = reader.readLine()) != null) {
                 buffer.append(line);
             }
@@ -65,20 +76,9 @@ public class RateFetcher extends AsyncTask<URL, String, String> {
                 exchangeRate = rates.getDouble(country);
                 dataStore.saveRateData(country, exchangeRate.floatValue());
             }
-            return exchangeRate.toString();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return null;
+        return exchangeRate.toString();
     }
-
-    protected void onPostExecute(String result) {
-        super.onPostExecute(result);
-        progressDialog.dismiss();
-    }
-
 }
