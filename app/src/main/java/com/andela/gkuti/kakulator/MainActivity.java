2aba71e662a1dcde18fcb0b757473f47b14b722b
andela-gkuti <kuti.gbolahan@andela.com>
2016-04-22 08:12:15 +0100

    Inflate action bar with settings icon and handle click events

--- app/src/main/java/com/andela/gkuti/kakulator/MainActivity.java
+++ app/src/main/java/com/andela/gkuti/kakulator/MainActivity.java
@@ -1,6 +1,10 @@
 package com.andela.gkuti.kakulator;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -24,6 +28,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initializeComponent();
         rateFetcher.execute();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsAcitivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void initializeComponent() {
@@ -73,4 +95,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }
