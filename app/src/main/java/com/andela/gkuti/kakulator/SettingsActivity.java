f91074d3169d77dcd813f75147a14cfc73e02f7e
andela-gkuti <kuti.gbolahan@andela.com>
2016-05-01 07:52:14 +0100

    Rename SettingsActivity class

--- app/src/main/java/com/andela/gkuti/kakulator/SettingsActivity.java
+++ app/src/main/java/com/andela/gkuti/kakulator/SettingsActivity.java
@@ -9,7 +9,7 @@ import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
-public class SettingsAcitivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, CompoundButton.OnCheckedChangeListener {
+public class SettingsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, CompoundButton.OnCheckedChangeListener {
     private SpinnerHelper spinner;
     private DataStore dataStore;
     private TextView textView;
@@ -25,10 +25,10 @@ public class SettingsAcitivity extends AppCompatActivity implements AdapterView.
 
     public void initializeSpinner() {
         final String[] abbreviations = getResources().getStringArray(R.array.Abbreviations);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(SettingsAcitivity.this,
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(SettingsActivity.this,
                 android.R.layout.simple_spinner_item, countries);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner = new SpinnerHelper(findViewById(R.id.spinner2));
+        spinner = new SpinnerHelper(findViewById(R.id.settings_spinner));
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
     }
