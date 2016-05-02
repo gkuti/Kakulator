bf793074a81cfbbc6982ff30727c4fa15535d7b1
andela-gkuti <kuti.gbolahan@andela.com>
2016-05-02 08:21:07 +0100

    Create Constants enum

---
+++ app/src/main/java/com/andela/gkuti/kakulator/Constants.java
@@ -0,0 +1,17 @@
+package com.andela.gkuti.kakulator;
+
+public enum Constants {
+
+    API_URL("http://openexchangerates.org/api/latest.json?"),
+    APP_TOKEN("app_id=a4c2366df7e347a4a5b5d0594bb8e41f"),
+    DATA_FILENAME("Kakulator");
+    String value;
+
+    Constants(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
