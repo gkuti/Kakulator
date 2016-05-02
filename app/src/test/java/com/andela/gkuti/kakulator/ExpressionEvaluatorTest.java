43a9cc5de0090a3096ef445dd24ee9ac985fb0d3
andela-gkuti <kuti.gbolahan@andela.com>
2016-05-02 09:34:18 +0100

    [Starts #118080221)] Add ExpressionEvaluatorTest

--- app/src/test/java/com/andela/gkuti/kakulator/ExpressionEvaluatorTest.java
+++ app/src/test/java/com/andela/gkuti/kakulator/ExpressionEvaluatorTest.java
@@ -3,13 +3,11 @@ package com.andela.gkuti.kakulator;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
-/**
- * Created by andela-jugba on 5/2/16.
- */
 public class ExpressionEvaluatorTest {
-
     @Before
     public void setUp() throws Exception {
 
@@ -17,21 +15,11 @@ public class ExpressionEvaluatorTest {
 
     @Test
     public void testEvaluate() throws Exception {
-
-    }
-
-    @Test
-    public void testSimplify() throws Exception {
-
-    }
-
-    @Test
-    public void testEval() throws Exception {
-
-    }
-
-    @Test
-    public void testGenerateExpression() throws Exception {
-
+        double result = ExpressionEvaluator.evaluate("5 * 5 - 2 * 5 + 3");
+        assertEquals(18.0, result, 0);
+        double result2 = ExpressionEvaluator.evaluate("5 + 5 - 15 / 5 + 3");
+        assertEquals(10.0, result2, 0);
+        double result3 = ExpressionEvaluator.evaluate("8 * 6 - 49 / 7 + 3 * 6 - 13 + 9 / 3");
+        assertEquals(49.0, result3, 0);
     }
 }
